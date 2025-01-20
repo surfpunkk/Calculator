1b3947bcf23b162eb165ee5dfcff1886402508fb
surfpunkk <krivoseev273@gmail.com>
2025-01-20 23:36:03 +0700

    Незначительные изменения

--- Calculator.cpp
+++ Calculator.cpp
@@ -447,7 +447,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
 }
 
 int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("com.example.calculator", G_APPLICATION_DEFAULT_FLAGS);
+    GtkApplication *app = gtk_application_new("com.github.surfpunkk.calculator", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     int status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
